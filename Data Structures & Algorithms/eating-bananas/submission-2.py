8f21a2c70d68f9c8b352d05de4de574da4aa857f
agarw503 <agarw503@purdue.edu>
2026-09-21 16:38:05 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        l , r = 1, max(piles)
+        res = r
+        while l <= r:
+            mid = (l+r) // 2
+            ttime = 0
+            for p in piles:
+                ttime += math.ceil(p / mid)
+            if ttime <= h:
+                res = mid
+                r = mid -1 
+            else:
+                l = mid +1
+        return res
+
+
+
+        
